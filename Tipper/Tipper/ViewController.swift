0e61c03ce1195661349655cb1e4f15bae95d16ff
Alberto Nencioni <alberto.nencioni97@gmail.com>
2018-10-17 13:38:55 -0700

    Update .gitignore

---
+++ Tipper/Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Alberto on 10/17/18.
+//  Copyright © 2018 Alberto Nencioni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
